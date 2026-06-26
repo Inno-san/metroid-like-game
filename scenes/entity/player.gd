11e982a53b667dfe4de70ed7918841ad99d5b39a
Amr Akram <darkwizard6942@gmail.com>
2026-06-26 18:39:35 +0300

    moving animation added

--- scenes/entity/player.gd
+++ scenes/entity/player.gd
@@ -1,11 +1,22 @@
 extends CharacterBody2D
 
-@export var jump_height:= -300
+@export var jump_height:= -500
 @export var gravity:=1000
 @export var speed:=150
 @export var direction_x: float 
 
 signal shoot(pos:Vector2, dir:Vector2)
+
+const gun_directions = {
+	Vector2i(1,0): 0,
+	Vector2i(1,1): 1,
+	Vector2i(0,1): 2,
+	Vector2i(-1,1): 3,
+	Vector2i(-1,0): 4,
+ 	Vector2i(-1,-1): 5,
+	Vector2i(0,-1): 6,
+	Vector2i(1,-1): 7,
+}
 
 func get_input():
 	direction_x= Input.get_axis("left","right")
@@ -28,4 +39,14 @@ func _physics_process(delta: float) -> void:
 	get_input()
 	velocity.x= direction_x * speed
 	Apply_gravity(delta)
+	animation()
 	move_and_slide()
+
+func animation():
+	$legs.flip_h =direction_x<0
+	$AnimationPlayer.current_animation= 'run' if direction_x else 'idle'
+	if is_on_floor() ==false:
+		$AnimationPlayer.play("jump")
+	var raw_dir = get_local_mouse_position().normalized()
+	var adjusted_dir = Vector2i(round(raw_dir.x),round(raw_dir.y))
+	$torso.frame =gun_directions[adjusted_dir]
